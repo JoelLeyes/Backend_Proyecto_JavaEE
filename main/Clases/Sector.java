c869580c8ca81e0d4ca779ab6b4c639aed294f2c
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-04-11 14:17:19 -0300

    Update sector

--- main/Clases/Sector.java
+++ main/Clases/Sector.java
@@ -1,2 +1,15 @@
-public enum Sector {
+public class Sector {
+    private String nombre;
+
+    public Sector(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
