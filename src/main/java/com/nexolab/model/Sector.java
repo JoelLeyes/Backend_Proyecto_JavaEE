2e7343330f8e5c7d3aa0531c24a262aa323a7380
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-04-16 19:44:04 -0300

    Revert "cambios admin"
    This reverts commit ffc3883b25a0d6e66f243efa17313f2c66b8705b.

--- src/main/java/com/nexolab/model/Sector.java
+++ src/main/java/com/nexolab/model/Sector.java
@@ -1,26 +1,7 @@
 package com.nexolab.model;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "sector")
-public class Sector {
-	@Id
-	@Column(nullable = false, unique = true)
-	private String name;
-
-	public Sector() {
-	}
-
-	public Sector(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+public enum Sector {
+	SIN_SECTOR,
+	RRHH,
+	FINANZAS
 }
