74244a87461467c526a1dbe0bd6602792ad46b06
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-04-16 19:44:24 -0300

    Revert "cambios"
    This reverts commit 1d626312bc9a83b1727bf9a6d27bb349e9adc728.

--- src/main/java/com/nexolab/service/ChatService.java
+++ src/main/java/com/nexolab/service/ChatService.java
@@ -1,10 +1,9 @@
 package com.nexolab.service;
+
 import com.nexolab.dao.ChatDAO;
 import com.nexolab.model.Chat;
-import com.nexolab.model.TipoChat;
 import com.nexolab.model.Usuario;
 import java.util.List;
-import java.util.Set;
 
 public class ChatService {
 	private ChatDAO chatDAO = new ChatDAO();
@@ -15,14 +14,5 @@ public class ChatService {
 
 	public Chat obtenerChatPorId(Long id) {
 		return chatDAO.findById(id);
-	}
-
-	public Chat crearChat(String name, TipoChat type, Set<Usuario> participantes) {
-		Chat chat = new Chat();
-		chat.setName(name);
-		chat.setType(type);
-		chat.setParticipantes(participantes);
-		chatDAO.save(chat);
-		return chat;
 	}
 }
