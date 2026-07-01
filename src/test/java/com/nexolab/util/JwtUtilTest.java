98e1887c7cd5f8dade9290ab51aa65c1a9790ddb
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-07-01 13:08:30 -0300

    Update JwtUtilTest.java

--- src/test/java/com/nexolab/util/JwtUtilTest.java
+++ src/test/java/com/nexolab/util/JwtUtilTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JwtUtilTest {
-
+    //
     @Test
     void generateTokenAndReadUserId() {
         Long expectedUserId = 123L;
